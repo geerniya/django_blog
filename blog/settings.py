bd9f571325319dba986cf57a2492b669e1940de6
geerniya <31003770+geerniya@users.noreply.github.com>
2018-04-16 17:19:16 +0800

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'myblog',
         'USER': 'root',
-        'PASSWORD': 'sunshuchang',
+        'PASSWORD': 'password',
         'HOST': '127.0.0.1',
         'PORT': '3306'
     }
